ad82b1620d46f612d6c49632d93c2c27c53bfaac
roslynpius <roslyn.pius@gmail.com>
2023-12-13 19:35:47 +0530

    Added: UC 4- Create an Employee Payroll Service to store Employee Payroll into a File

--- src/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
+++ src/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -1,15 +1,22 @@
 package com.bridgelabz.employeepayrollservice;
 
-public class EmployeePayrollService {
+import java.io.*;
+
+/**
+ * Represents the Employee Payroll Service class that stores employee details and provides file operations.
+ */
+public class EmployeePayrollService implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private String employeeId;
     private String employeeName;
     private long employeeSalary;
 
     /**
-     * @desc Parametrized constructor to initialize the values of the object of EmployeePayrollService class
-     * @param employeeId ID of employee
+     * Constructor to initialize employee details
+     * @param employeeId Employee ID
      * @param employeeName Employee Name
-     * @param employeeSalary Salary of Employee
+     * @param employeeSalary Employee Salary
      */
     public EmployeePayrollService(String employeeId, String employeeName, long employeeSalary) {
         this.employeeId = employeeId;
@@ -18,15 +25,46 @@ public class EmployeePayrollService {
     }
 
     /**
-     * @desc Method to print all the Details of the Employee
-     * @return string to be printed containing all details of employee
+     * @desc Returns a string representation of the EmployeePayrollService object.
+     * @return A string containing employee details.
      */
     @Override
     public String toString() {
         return "{" +
-                "employeeId=" + employeeId + ",\n" +
-                "employeeName=" + employeeName + ",\n" +
-                "employeeSalary=" + employeeSalary + ",\n" +
+                "employeeId='" + employeeId + '\'' +
+                ", employeeName='" + employeeName + '\'' +
+                ", employeeSalary=" + employeeSalary +
                 '}';
+    }
+
+    /**
+     * @desc Writes the employee data to a file in CSV format.
+     * @param fileName The name of the file to write to.
+     */
+    public void writeToFile(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(employeeId + "," + employeeName + "," + employeeSalary);
+            writer.newLine();  // Add a new line for each entry
+            System.out.println("Employee data written to file: " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @desc Counts the number of entries in the specified file.
+     * @param fileName The name of the file to count entries in.
+     * @return The number of entries in the file.
+     */
+    public static int countEntries(String fileName) {
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.readLine() != null) {
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
     }
 }
--- src/com/bridgelabz/employeepayrollservice/Main.java
+++ src/com/bridgelabz/employeepayrollservice/Main.java
@@ -1,24 +1,48 @@
 package com.bridgelabz.employeepayrollservice;
+
+import java.io.File;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Employee Payroll Implementation");
 
         Scanner input = new Scanner(System.in);
-        String employeeId , employeeName;
+        String employeeId, employeeName;
         long employeeSalary;
-        System.out.println("Enter employee id : ");
+        System.out.println("Enter employee id: ");
         employeeId = input.next();
-        System.out.println("Enter employee name in a single line : ");
+        System.out.println("Enter employee name in a single line: ");
         input.nextLine();
         employeeName = input.nextLine();
-        System.out.println("Enter employee salary : ");
+        System.out.println("Enter employee salary: ");
         employeeSalary = input.nextLong();
 
-        EmployeePayrollService employee = new EmployeePayrollService(employeeId,employeeName,employeeSalary);
+        EmployeePayrollService employee = new EmployeePayrollService(employeeId, employeeName, employeeSalary);
 
-        System.out.println("Employee payroll data : " + employee);
+        // Specify the folder name where you want to store the employee details
+        String folderName = "database";
+        File folder = new File(folderName);
+
+        // Create the "database" folder if it doesn't exist
+        if (!folder.exists()) {
+            if (folder.mkdir()) {
+                System.out.println("Folder created: " + folder.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create folder");
+                return;
+            }
+        }
+
+        // Specify the file path within the "database" folder
+        String fileName = folderName + File.separator + "employee_data.txt";
+
+        // Write employee data to the file
+        employee.writeToFile(fileName);
+
+        // Count the number of entries in the file
+        int entryCount = EmployeePayrollService.countEntries(fileName);
+        System.out.println("Number of entries in the file: " + entryCount);
     }
 }
